d65b390af001982ab2bca7932e0af9c555fed363
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-15 14:23:14 -0600

    find, create, update, and delete operations for the Tag model

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -11,18 +11,56 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, { 
+      include: [{ model: Product }],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'Sorry, There is no tag with this ID!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'Sorry, There is no tag with this ID!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'Sorry, There is no tag with this ID!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  }
 });
 
 module.exports = router;
