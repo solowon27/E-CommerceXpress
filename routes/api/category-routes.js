15978aed26d88833f3d26290183706f725e66d22
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-15 14:22:08 -0600

    error handling included for each route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,7 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
@@ -11,10 +11,12 @@ router.get('/', (req, res) => {
       include: [{ model: Product }],
     });
     res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 try {
@@ -30,15 +32,17 @@ try {
 }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
     const categoryData = await Category.update(req.body, {
@@ -51,11 +55,12 @@ router.put('/:id', (req, res) => {
       return;
     }
     res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
   }
-
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
@@ -68,6 +73,8 @@ router.delete('/:id', (req, res) => {
       return;
     }
     res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
