09109de0c1dc101ed3017da2341d38c89686a0f8
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-15 11:39:41 -0600

    catagory_id column added to Product model

--- models/Product.js
+++ models/Product.js
@@ -34,6 +34,14 @@ Product.init(
             isNumeric: true
             }
          },
+    category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'category',
+            key: 'id'
+        }
+    }
+  },
   {
     sequelize,
     timestamps: false,
