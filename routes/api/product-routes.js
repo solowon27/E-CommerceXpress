074fd6d7e1fad0036fd28e0d905b8d710c26b901
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-15 13:59:56 -0600

    findAll and findByPk methods used for get request

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }], 
+    });
+    res.status(200).json(productData);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'Sorry, There is no product with this ID!' });
+      return;
+    }
+    res.status(200).json(productData);
+  }
 });
 
 // create new product
@@ -91,6 +107,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'Sorry, There is no product with this ID!' });
+      return;
+    }
+    res.status(200).json(productData);
+  }
 });
 
 module.exports = router;
