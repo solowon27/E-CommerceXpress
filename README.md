a8d4af0d6f68fe421857c40d435e1834149c0566
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-17 16:08:39 -0600

    get routes for tags get updated

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # E-CommerceXpress
+
+## description
 E-CommerceXpress is a robust and efficient e-commerce back end built using Express.js, Sequelize, and MySQL. This repository contains the code for configuring a working Express.js API that utilizes Object-Relational Mapping (ORM) techniques to interact with a MySQL database. 
+
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product} = require('../../models');
 
 // The `/api/tags` endpoint
 
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
 try {
   const tagData = await Tag.findAll({
-    include: [{ model: ProductTag }, { model: Product }],
+    include: [{ model: Product }],
   });
   res.status(200).json(tagData);
 } catch (err) {
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, { 
-      include: [{ model: ProductTag }, { model: Product }],
+      include: [{ model: Product }],
     });
     if (!tagData) {
       res.status(404).json({ message: 'Sorry, There is no tag with this ID!' });
